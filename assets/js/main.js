510325090658c78cc94a74bbda36f6ed9936f65a
Kang byeonghyeon <black1663@naver.com>
2020-12-22 12:37:10 +0900

    JS: Handle submit

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,7 +1,27 @@
 'use strict'
 
+const itemForm = document.querySelector('.shopping-form'),
+  itemInput = itemForm.querySelector('.shopping-form__input'),
+  itemAddBtn = itemForm.querySelector('.shopping-form__add-btn');
+
+const ITEM_LS = 'item';
+
+function handleSubmit(e) {
+  e.preventDefault();
+  const currentValue = itemInput.value;
+  paintItem(curentValue);
+}
+
+function loadItems() {
+  const loadedItems = localStorage.getItem(ITEM_LS);
+  if(loadedItems !== null) {
+
+  }
+}
+
 function init() {
-  
+  loadItems();
+  itemForm.addEventListener('submit', handleSubmit);
 }
 
 init();
